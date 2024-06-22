0257cd7996cce90ce8b70534f2d6d0395542fa33
Carlos Eduardo <cadu8344@gmail.com>
2024-06-22 19:31:01 -0300

    primeira view

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="/src/assets/images/logo.svg" alt="Logo do cookin Up" class="logo">
+      <p class="cabecalho-lh frase-cabecalho">
+        <span class="texto-verde">Um banquete para </span>
+        despertar o chef que há em você
+      </p>
+
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que 
+        estão ao seu alcance!
+      </p>
+    </div>
+
+    <img src="/src/assets/images/fundo-banner.png" alt="Foto de  um mulher cozinhando com uma 
+    bacia de vidro nas mãos" class="foto-banner">
+
+  </header>
+</template>
+
